c223b82928487d07d82f35d8239c36a8e33efebf
wirless63 <46608076+wirless63@users.noreply.github.com>
2026-09-18 15:03:01 -0600

    Update firmware build date and version in MyMesh.h

--- examples/simple_repeater/MyMesh.h
+++ examples/simple_repeater/MyMesh.h
@@ -71,11 +71,11 @@ struct NeighbourInfo {
 };
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "14 Aug 2026"
+  #define FIRMWARE_BUILD_DATE   "11 Sep 2026"
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.17.1"
+  #define FIRMWARE_VERSION   "PwrSave-v1.17.1"
 #endif
 
 #define FIRMWARE_ROLE "repeater"
